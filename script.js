fd240f48b31476283730122df8378a5f5b17e774
Piotr Nowakowski <nowakowski.peter@gmail.com>
2018-09-23 12:47:00 +0000

    Added sounds and playing sounds when a button is clicked

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 /* global $ */
 
+var playingDemo = false;
+var currentRound = 0 ;
 
+var sounds = {
+    a: new Audio('sounds/a.mp3'),
+    b: new Audio('sounds/b.mp3'),
+    c: new Audio('sounds/c.mp3'),
+    d: new Audio('sounds/d.mp3')
+}
 
 $(document).ready(function () {
+    
+    // clicks will do nothing if the script is currently playing the sequence
+    if (playingDemo) {
+        return;
+    }
     $( "#a" ).mousedown(function() {
-      $('#a').css('backgroundColor', 'red');
+        $('#a').css('backgroundColor', 'red');
     });
     $( "#b" ).mousedown(function() {
-      $('#b').css('backgroundColor', 'blue');
+        $('#b').css('backgroundColor', 'blue');
     });
     $( "#c" ).mousedown(function() {
-      $('#c').css('backgroundColor', 'orange');
+        $('#c').css('backgroundColor', 'orange');
     });
     $( "#d" ).mousedown(function() {
-      $('#d').css('backgroundColor', 'lightgreen');
+        $('#d').css('backgroundColor', 'lightgreen');
     });
-      $('.game-div').mouseup(function (){
+    $('.game-div').mouseup(function (){
         $(this).css('backgroundColor', 'white');
     });
-
+    $('.game-div').click(function (){
+        var id = $(this).attr('id') ;
+        sounds[id].play();
+    });
 
 
 });
+
+function playNextRound(currentRound) {
+    playingDemo = true ;
+    // put a script here that will play a sequence and set playingDemo = true, once it's finished
+}
